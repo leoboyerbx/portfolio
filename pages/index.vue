03ac7c546f1dd0686c5275a18d310ffd082d5a4a
leoboyerbx <contact@leoboyer.fr>
2024-02-22 17:19:13 +0100

    feat: add meta description

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
 <template>
   <div>
     <Title>Léo Boyer - Creative Developer</Title>
+    <Meta
+      name="desription"
+      content="I'm Léo, a creative and curious developer. Currently working at Wanaka, Annecy FR."
+    />
     <Hero />
   </div>
 </template>
