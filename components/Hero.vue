e6e436c6e1a110a87d64ecbdb24bd20ec753c970
leoboyerbx <contact@leoboyer.fr>
2024-02-22 10:12:06 +0100

    wip hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts"></script>
 <template>
-  <header class="w-full bg-blue h-90vh px-1c">
-    <h1 class="">hi, this block is outlined</h1>
+  <header
+    class="w-full bg-slate-900 h-100vh px-1c text-white grid place-content-center"
+  >
+    <h1 class="font-black text-24 leading-100%">
+      Hi, I'm Léo<br />a curious developer
+    </h1>
   </header>
 </template>
