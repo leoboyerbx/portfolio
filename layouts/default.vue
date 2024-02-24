32e14db4b52b75cf29bbe77f45cd4edbcbcd7c97
leoboyerbx <contact@leoboyer.fr>
2024-02-24 21:10:11 +0100

    Move common markun-p to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,22 @@ const { locale } = useI18n()
         />
       </Head>
       <Body>
-        <slot />
+        <div class="relative overflow-clip">
+          <img
+            src="~/assets/img/hero.webp"
+            alt=""
+            class="pointer-events-none absolute left-0 block h-300vh w-full blur -top-6"
+          />
+          <img
+            src="~/assets/img/hero.webp"
+            alt=""
+            class="pointer-events-none absolute left-0 z-0 block h-200vh w-full rotate-180 opacity-80 blur grayscale -bottom-6"
+          />
+          <main class="relative z-10">
+            <slot />
+          </main>
+          <Footer />
+        </div>
       </Body>
     </Html>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="relative overflow-clip">
-    <img
-      src="~/assets/img/hero.webp"
-      alt=""
-      class="pointer-events-none absolute left-0 block h-300vh w-full blur -top-6"
-    />
-    <img
-      src="~/assets/img/hero.webp"
-      alt=""
-      class="pointer-events-none absolute left-0 z-0 block h-200vh w-full rotate-180 opacity-80 blur grayscale -bottom-6"
-    />
-    <Hero class="relative z-10" />
-    <main class="relative z-10 flex flex-col gap-64">
-      <ProjectList />
-      <OpenSourceList />
-      <ContactPush />
-    </main>
-    <Footer />
+  <div class="flex flex-col gap-64">
+    <Hero />
+    <ProjectList />
+    <OpenSourceList />
+    <ContactPush />
   </div>
 </template>
