19337fd77fa3b4dee474350e72847ad4db252673
leoboyerbx <contact@leoboyer.fr>
2024-02-24 22:27:48 +0100

    Feat: embedplayer component

---
+++ components/EmbedPlayer.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps<{
+  url: string
+  thumbnailUrl: string
+}>()
+const urlWithAutoplay = computed(() => {
+  const url = new URL(props.url)
+  url.searchParams.set('autoplay', '1')
+  return url.toString()
+})
+const loaded = ref(false)
+</script>
+<template>
+  <div
+    class="relative aspect-video overflow-hidden border border-slate-500/50 rounded-xl bg-gray-950"
+  >
+    <iframe
+      v-if="loaded"
+      :src="urlWithAutoplay"
+      frameborder="0"
+      class="absolute inset-0 h-full w-full"
+      allowfullscreen
+    ></iframe>
+    <div
+      v-else
+      class="absolute inset-0 grid h-full w-full place-content-center"
+      @click="loaded = true"
+    >
+      <img
+        :src="thumbnailUrl"
+        alt="Video Thumbnail"
+        class="absolute inset-0 h-full w-full object-cover opacity-90"
+        @load="loaded = true"
+      />
+      <button
+        class="z-1 grid h-16 w-16 place-content-center rounded-full bg-slate-800/30"
+      >
+        <span class="i-uil:play ml-1 block text-28px"></span>
+      </button>
+    </div>
+  </div>
+</template>
+<style scoped></style>
