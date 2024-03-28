62be53745d99255f5c8c8a4dd244cf4b001c2672
leoboyerbx <contact@leoboyer.fr>
2024-03-28 20:28:23 +0100

    update pm2 config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,7 +4,10 @@ module.exports = {
             name: 'PortfolioAdmin',
             port: '1337',
             script: "npm",
-            args: "start"
+            args: "start",
+            env: {
+                "NODE_ENV": "development",
+            }
         },
     ],
 }
