37e2ab6025d7900cb9683754efb5f6fb7b099efb
leoboyerbx <contact@leoboyer.fr>
2024-03-28 19:49:46 +0100

    redo

--- client/server/api/resume.ts
+++
@@ -1,9 +0,0 @@
-import { StaticData } from '~/types/apiTypes'
-
-export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig(event)
-  const { data } = (await $fetch(config.public.apiUrl + '/items/static')) as {
-    data: StaticData
-  }
-  return proxyRequest(event, config.public.apiUrl + '/assets/' + data.resume)
-})
