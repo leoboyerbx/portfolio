c5510d65e0435242839343420349e751d72c9575
leoboyerbx <contact@leoboyer.fr>
2024-05-31 11:45:31 +0200

    accessibility: buttons titles

--- client/components/MainNav.vue
+++ client/components/MainNav.vue
@@ -107,6 +107,7 @@ const mounted = useMounted()
     >
       <button
         ref="backEl"
+        :aria-label="$t('nav.backToHome')"
         class="child-transition btn-animation block flex-shrink-0 px-3.5"
         :style="{
           transform: isHome ? `translateX(-100%)` : '',
@@ -131,6 +132,7 @@ const mounted = useMounted()
         >
           <button
             class="nav-link btn-animation grid h-7 w-7 flex-shrink-0 place-content-center"
+            :aria-label="$t('nav.openMenu')"
             @click="mobileMenuOpen = !mobileMenuOpen"
           >
             <span
