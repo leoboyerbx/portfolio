6238597dc46159b22bb796a27ddee9da63a84e32
leoboyerbx <contact@leoboyer.dev>
2025-04-22 23:05:01 +0200

    wip

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -4,25 +4,25 @@ export default defineNuxtSchema({
     fr: group({
         title: 'French',
         description: 'French config',
-        icon: 'i-material-symbols:language-french-rounded',
+        // icon: 'i-material-symbols:language-french-rounded',
         fields: {
             resume: group({
                 title: 'Resume',
                 description: 'Resume config',
-                icon: 'i-uil:file-alt',
+                // icon: 'i-uil:file-alt',
                 fields: {
                     title: field({
                         type: 'string',
                         title: 'Title',
                         description: 'Displayed title',
-                        icon: 'i-uil:text-fields',
+                        // icon: 'i-uil:text-fields',
                         default: 'CV',
                     }),
                     file: field({
                         type: 'media',
                         title: 'Resume PDF file',
                         description: 'Upload the resume file.',
-                        icon: 'i-uil:file-upload',
+                        // icon: 'i-uil:file-upload',
                     }),
                 },
             }),
