22a92f3f2e764ccf1253c87357ef12cfcfa9c083
leoboyerbx <contact@leoboyer.dev>
2025-04-26 14:37:01 +0200

    fix appconfig

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,31 +1,33 @@
 import { field, group } from '@nuxt/content/preview'
 
 export default defineNuxtSchema({
-    fr: group({
-        title: 'French',
-        description: 'French config',
-        icon: 'i-material-symbols-language-french-rounded',
-        fields: {
-            resume: group({
-                title: 'Resume',
-                description: 'Resume config',
-                icon: 'i-uil-file-alt',
-                fields: {
-                    title: field({
-                        type: 'string',
-                        title: 'Title',
-                        description: 'Displayed title',
-                        icon: 'i-uil:text-fields',
-                        default: 'CV',
-                    }),
-                    file: field({
-                        type: 'media',
-                        title: 'Resume PDF file',
-                        description: 'Upload the resume file.',
-                        icon: 'i-uil-file-upload',
-                    }),
-                },
-            }),
-        },
-    }),
+    appConfig: {
+        fr: group({
+            title: 'French',
+            description: 'French config',
+            icon: 'i-material-symbols-language-french-rounded',
+            fields: {
+                resume: group({
+                    title: 'Resume',
+                    description: 'Resume config',
+                    icon: 'i-uil-file-alt',
+                    fields: {
+                        title: field({
+                            type: 'string',
+                            title: 'Title',
+                            description: 'Displayed title',
+                            icon: 'i-uil:text-fields',
+                            default: 'CV',
+                        }),
+                        file: field({
+                            type: 'media',
+                            title: 'Resume PDF file',
+                            description: 'Upload the resume file.',
+                            icon: 'i-uil-file-upload',
+                        }),
+                    },
+                }),
+            },
+        }),
+    },
 })
