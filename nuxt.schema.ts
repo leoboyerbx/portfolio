dc92904f7c9bb8ab6fd2ff1d8ea5b2ec2c8c419c
leoboyerbx <contact@leoboyer.dev>
2025-04-26 14:48:17 +0200

    Wip on app config schema

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -2,32 +2,52 @@ import { field, group } from '@nuxt/content/preview'
 
 export default defineNuxtSchema({
     appConfig: {
-        fr: group({
-            title: 'French',
-            description: 'French config',
-            icon: 'i-material-symbols-language-french-rounded',
+        parent: group({
+            title: 'UI',
+            description: 'UI configuration',
+            icon: 'i-ph-palette-fill',
             fields: {
-                resume: group({
-                    title: 'Resume',
-                    description: 'Resume config',
-                    icon: 'i-uil-file-alt',
-                    fields: {
-                        title: field({
-                            type: 'string',
-                            title: 'Title',
-                            description: 'Displayed title',
-                            icon: 'i-uil:text-fields',
-                            default: 'CV',
-                        }),
-                        file: field({
-                            type: 'media',
-                            title: 'Resume PDF file',
-                            description: 'Upload the resume file.',
-                            icon: 'i-uil-file-upload',
-                        }),
-                    },
+                primary: field({
+                    type: 'string',
+                    title: 'Primary',
+                    description: 'Primary color of your UI.',
+                    icon: 'i-ph-palette',
+                    default: 'sky',
+                    required: ['sky', 'mint', 'rose', 'amber'],
                 }),
             },
         }),
     },
 })
+
+// export default defineNuxtSchema({
+//     appConfig: {
+//         fr: group({
+//             title: 'French',
+//             description: 'French config',
+//             icon: 'i-material-symbols-language-french-rounded',
+//             fields: {
+//                 resume: group({
+//                     title: 'Resume',
+//                     description: 'Resume config',
+//                     icon: 'i-uil-file-alt',
+//                     fields: {
+//                         title: field({
+//                             type: 'string',
+//                             title: 'Title',
+//                             description: 'Displayed title',
+//                             icon: 'i-uil:text-fields',
+//                             default: 'CV',
+//                         }),
+//                         file: field({
+//                             type: 'media',
+//                             title: 'Resume PDF file',
+//                             description: 'Upload the resume file.',
+//                             icon: 'i-uil-file-upload',
+//                         }),
+//                     },
+//                 }),
+//             },
+//         }),
+//     },
+// })
